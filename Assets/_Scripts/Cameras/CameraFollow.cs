ee2be7a818fd3b1145ea0bf7ae98d53458275bdc
twinkocat <msbulahmaxim@gmail.com>
2023-05-01 10:02:24 +0300

    Adding wrap effect in Scene

--- Assets/_Scripts/Cameras/CameraFollow.cs
+++ Assets/_Scripts/Cameras/CameraFollow.cs
@@ -5,10 +5,12 @@ public class CameraFollow : MonoBehaviour
     #region Params
     [Header("Set in Inspector")]
     public GameObject           Player;
+    public GameObject           wrapEffectPrefab;
 
     private float               smoothTime = 0.35f;
     private Vector3             offset = new Vector3 (3f, 6f, -10f);
     private Vector3             velocity = Vector3.zero;
+    private Vector3             wrapEffectOffset = new Vector3(0, 0, 10f);
     #endregion
 
     #region Initialization
@@ -18,6 +20,11 @@ public class CameraFollow : MonoBehaviour
         {
             print("Set player in Inspector");
         }
+        if(wrapEffectPrefab == null)
+        {
+            print("Set wrapEffect in Inspector");
+        }
+        wrapEffectPrefab = Instantiate(wrapEffectPrefab);
     }
     #endregion
 
@@ -27,6 +34,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = Player.transform.position + offset;
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
         transform.position = smoothedPosition;
+        wrapEffectPrefab.transform.position = transform.position + wrapEffectOffset;
         transform.LookAt(Player.transform.position);
     }
     #endregion
