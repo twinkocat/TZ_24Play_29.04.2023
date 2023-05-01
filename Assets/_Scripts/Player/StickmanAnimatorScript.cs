912dcd103b4e05e526701af9103f094775d29770
twinkocat <msbulahmaxim@gmail.com>
2023-05-01 10:04:55 +0300

    Try to add animaition. Create example how this should work

--- Assets/_Scripts/Player/StickmanAnimatorScript.cs
+++ Assets/_Scripts/Player/StickmanAnimatorScript.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class StickmanAnimatorScript : MonoBehaviour
 {
-    
+    public static StickmanAnimatorScript s_StickmanAnimatorScript;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        if (s_StickmanAnimatorScript != null)
+        {
+            print("StickmanAnimatorScript: Instance already set. It should be Singleton");
+            return;
+        }
+        s_StickmanAnimatorScript = this;
+        _animator = GetComponent<Animator>();
+    }
+
+    public void AnimatorTriggerJump()
+    {
+        // have bugs 
+        //_animator.SetTrigger("Jump");
+    }
 }
